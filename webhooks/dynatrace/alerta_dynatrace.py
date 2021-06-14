7833632dc4c6dd3e023f53caa8084e9af253f56a
novicejava1 <sudhirbhoga@gmail.com>
2021-06-14 09:34:28 +0000

    Fix the return data

--- webhooks/dynatrace/alerta_dynatrace.py
+++ webhooks/dynatrace/alerta_dynatrace.py
@@ -7,23 +7,6 @@ class DynatraceWebhook(WebhookBase):
 
     def incoming(self, query_string, payload):
 
-        data = json.load(payload)
-        resource = data['ImpactedEntities'][0]['entity']
-        event = data['ProblemTitle']
-        environment = data['ProblemImpact']
-        severity = data['PID']
-        correlate = []
-        status = data['State']
-        service = data['ImpactedEntities'][1]['entity']
-        group = "Web Application"
-        value = data['ProblemTitle']
-        text = "High Alert"
-        tags = data['Tags']
-        attributes = []
-        origin = []
-        alerttype = []
-        raw_data = data['Problem URL']
-
         return Alerta(
             resource = data['ImpactedEntities'][0]['entity'],
             event = data['ProblemTitle'],
