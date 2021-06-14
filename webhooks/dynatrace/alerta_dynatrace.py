3a408e02b8960befb65f9deb9bb7c69784b26a7a
novicejava1 <sudhirbhoga@gmail.com>
2021-06-14 12:04:58 +0000

    Fix

--- webhooks/dynatrace/alerta_dynatrace.py
+++ webhooks/dynatrace/alerta_dynatrace.py
@@ -13,7 +13,7 @@ class DynatraceWebhook(WebhookBase):
             environment = 'Production',
             severity= 'critical',
             #status = payload['State']
-            service = payload['ImpactedEntities'][1]['entity'].split('_')[0]
+            service = payload['ImpactedEntities'][1]['entity'].split('-')[0]
             #group = 'Web Application',
             #value = payload['ProblemTitle'],
             #text = 'High Alert',
